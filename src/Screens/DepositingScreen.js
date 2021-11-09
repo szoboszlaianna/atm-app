f3e17781c8246a068ef7d81143b323210cf155eb
szoboszlaianna <szoboszlai.anna0@gmail.com>
2021-11-09 10:04:37 +0100

    Header comp

--- src/Screens/DepositingScreen.js
+++ src/Screens/DepositingScreen.js
@@ -9,10 +9,15 @@ const DepositingScreen = props => {
   return (
     <>
       <div className={classes.header}>
-        <Key id='return' className={classes.return} onClick={props.onReturn}>
-          <img src='./back_arrow.png' alt='return' height='100%'></img>
-        </Key>
-        <Title className={classes.header}>{props.header}</Title>
+        <div className={classes.returnContainer}>
+          <Key id='return' className={classes.return} onClick={props.onReturn}>
+            <img src='./back_arrow.png' alt='return' height='100%'></img>
+          </Key>
+        </div>
+        <div className={classes.titleContainer}>
+          <Title className={classes.header}>{props.header}</Title>
+        </div>
+        <div className={classes.emptyContainer}></div>
       </div>
       <NumberField type='text' value={`£${props.amount}`} disabled='true'></NumberField>
       <CoinDisplay result={props.result} coins={props.coins} />
